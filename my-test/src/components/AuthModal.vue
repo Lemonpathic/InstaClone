03d0d0ddf7c21d6e4b5456c5527e9f9ad39d3b7b
Lemonpathic <goofymanbeek2.0@gmail.com>
2024-05-25 09:02:08 -0400

    modal messsages

--- my-test/src/components/AuthModal.vue
+++ my-test/src/components/AuthModal.vue
@@ -1,20 +1,51 @@
 <script setup>
-import { ref, shallowRef } from 'vue';
+import {reactive, ref, shallowRef} from 'vue';
+
 const props = defineProps(['isLogin']);
 import {useUserStore} from "@/stores/users.js";
+import {getAttributeValue} from "jsdom/lib/jsdom/living/attributes.js";
 
 const userStore = useUserStore()
 const {} = userStore
-console.log(props);
+
 const dialog = shallowRef(false);
 
-const rules = {
-  required: value => !!value
+const userCredentials = reactive({
+  fName:"",
+  lName:"",
+  age:0,
+  email:"",
+  username:"",
+  password:""
+})
+
+
+const handleOkay = (i) =>{
+  closeDialog()
+
 }
+
+const passwordRules = {
+  required: value => !!value,
+  length16: value => value.length > 8 || 'Password must be greater than 8 Characters',
+  matches: value => value === userCredentials.password || 'Passwords must match'
+}
+
+const closeDialog = () => {
+  dialog.value = false
+  userCredentials.password = ''
+}
+
+const handleDialogUpdate = (value) => {
+  if (!value) {
+    closeDialog();
+  }
+};
 </script>
 
 <template>
-  <v-dialog v-if="isLogin" v-model="dialog" width="400px">
+<!--  LOG IN MODAL-->
+  <v-dialog v-if="isLogin" v-model="dialog"  width="400px">
     <template v-slot:activator="{ props: activatorProps }">
       <v-btn text="Log In" v-bind="activatorProps" class="nav-auth-button"></v-btn>
     </template>
@@ -41,7 +72,9 @@ const rules = {
     </v-card>
   </v-dialog>
 
-  <v-dialog v-model="dialog" max-width="600" v-if="isLogin === false">
+
+<!--SIGN UP MODAL-->
+  <v-dialog @update:modelValue="handleDialogUpdate" v-model="dialog" max-width="600" v-if="isLogin === false">
     <template v-slot:activator="{ props: activatorProps }">
       <v-btn text="Sign Up" v-bind="activatorProps" class="nav-auth-button"></v-btn>
     </template>
@@ -62,10 +95,20 @@ const rules = {
             <v-text-field label="Email*" required></v-text-field>
           </v-col>
           <v-col cols="12" sm="6">
-            <v-text-field label="Password*" type="password" required></v-text-field>
+            <v-text-field
+                label="Password*"
+                type="password"
+                :rules="[passwordRules.required, passwordRules.length16]"
+                required
+            ></v-text-field>
           </v-col>
           <v-col cols="12" sm="6">
-            <v-text-field label="Confirm Password*" type="password" required></v-text-field>
+            <v-text-field
+                label="Confirm Password*"
+                type="password"
+                :rules="[passwordRules.matches]"
+                required
+            ></v-text-field>
           </v-col>
         </v-row>
 
@@ -76,8 +119,8 @@ const rules = {
 
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn text="Close" variant="plain" @click="dialog = false"></v-btn>
-        <v-btn color="primary" text="Create Account" variant="tonal" @click="dialog = false"></v-btn>
+        <v-btn text="Close" variant="plain" @click="closeDialog"></v-btn>
+        <v-btn color="primary" text="Create Account" variant="tonal" @click="handleOkay"></v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
